7bca86377c0f46ff5ff50d79afe79fb7ea65dacb
Michael Blades <michaelblades12@gmail.com>
2019-02-26 19:33:31 -0600

    Intital Commit

---
+++ CalCal/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  CalCal
+//
+//  Created by  on 2/26/19.
+//  Copyright © 2019 Michael Blades. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func buttonClick(_ sender: UIButton) {
+        
+    }
+    
+    
+
+}
+
