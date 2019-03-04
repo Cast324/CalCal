9843f43786a3e8bbd88b4445e933d481912efd55
Michael Blades <michaelblades12@gmail.com>
2019-03-04 13:54:10 -0600

    Started laying out operations
    Started Plus, Minus, Divide & Multiply will now save which operations they want to perform.

--- CalCal/ViewController.swift
+++ CalCal/ViewController.swift
@@ -12,6 +12,22 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var displayLabel: UILabel!
     
+    @IBOutlet weak var divideButton: UIButton!
+    @IBOutlet weak var multiplyButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    
+    enum operators {
+        case plus
+        case minus
+        case multiply
+        case divide
+        case none
+    }
+    
+    var nextOperator:operators = .none
+    var operatorToPerform:operators = .none
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -35,27 +51,54 @@ class ViewController: UIViewController {
         case "%":
             displayLabel.text = "%"
         case "÷":
-            displayLabel.text = "+"
+            nextOperator = .divide
+            sender.backgroundColor = UIColor.gray
         case "X":
-            displayLabel.text = "X"
+            nextOperator = .multiply
+            sender.backgroundColor = UIColor.gray
         case "-":
-            displayLabel.text = "-"
+            nextOperator = .minus
+            sender.backgroundColor = UIColor.gray
         case "+":
-            displayLabel.text = "+"
+            nextOperator = .plus
+            sender.backgroundColor = UIColor.gray
         case "=":
             displayLabel.text = "="
         case "<-":
             displayLabel.text = "BackSpace"
         default:
-            print("How did you do that?")
+            print("How did you do that? Error in Fuctions")
         }
     }
     
     func updateMainLabel(newNum: String) {
-        if displayLabel.text == "0" {
-            displayLabel.text = String(newNum)
+        if displayLabel.text == "0"{
+            displayLabel.text = newNum
+        } else if nextOperator == .none {
+            displayLabel.text?.append(newNum)
         } else {
-            displayLabel.text?.append(String(newNum))
+            displayLabel.text = newNum
+            switch nextOperator {
+            case .plus:
+                plusButton.backgroundColor = UIColor.white
+                nextOperator = .none
+                operatorToPerform = .plus
+            case .divide:
+                divideButton.backgroundColor = UIColor.white
+                nextOperator = .none
+                operatorToPerform = .divide
+            case .minus:
+                minusButton.backgroundColor = UIColor.white
+                nextOperator = .none
+                operatorToPerform = .minus
+            case .multiply:
+                multiplyButton.backgroundColor = UIColor.white
+                nextOperator = .none
+                operatorToPerform = .multiply
+            default:
+                print("Error When Update Main Label")
+                
+            }
         }
     }
     
